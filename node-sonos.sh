de0181d6f2bb0960624bc0bf8641263cbf1ec5bb
drohack <drohack@gmail.com>
2016-03-22 00:21:12 -0500

    fixed typo in node-sonos.sh

--- node-sonos.sh
+++ node-sonos.sh
@@ -2,8 +2,8 @@
 
 ### Open epiphany browser to point at the node-sonos-web-controller and hit F11 to fullscreen ###
 sleep 5s
-epiphany-broswer https://localhost:8080 &
+epiphany-browser https://localhost:8080 &
 sleep 10s
 xdotool key F11
 sleep 2s
-xdotool key F5 #refresh to make sure the page doesn't get sized incorrectly when full screened 
+xdotool key F5 #refresh to make sure the page doesn't get sized incorrectly when full screened
